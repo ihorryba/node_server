43394e435293d74aca6193841791a9222815cec5
Ihor Rybachok <iryba@softserveinc.com>
2018-11-16 18:59:57 +0200

    Next step is delete from DB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://max:38iPFGi8QxIhj4X7@cluster0-2yoxg.mongodb.net/node-angular?retryWrites=true')
+  .then(() => {
+    console.log('Connected to database!');
+  }).catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,30 +30,28 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
   });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  console.log(req.params.id);
+  res.status(200).json({ message: 'Post deleted!' });
 });
 
 module.exports = app;
